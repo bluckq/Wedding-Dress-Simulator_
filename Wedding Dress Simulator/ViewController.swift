408f3844f23466b50b083dfa16ed7ff5869fbddb
Aaron <bluckq@gmail.com>
2019-10-18 14:53:09 -0700

    Start

---
+++ Wedding Dress Simulator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Wedding Dress Simulator
+//
+//  Created by Aaron on 10/14/19.
+//  Copyright © 2019 Aaron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+       
+    }
+    
+
+}
+
---
+++ Wedding Dress SimulatorTests/Wedding_Dress_SimulatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Wedding_Dress_SimulatorTests.swift
+//  Wedding Dress SimulatorTests
+//
+//  Created by Aaron on 10/14/19.
+//  Copyright © 2019 Aaron. All rights reserved.
+//
+
+import XCTest
+@testable import Wedding_Dress_Simulator
+
+class Wedding_Dress_SimulatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
